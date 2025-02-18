c7418f4b82639d0397747ae173427ff947b5d288
baok3 <quanbaonguyen03@gmail.com>
2025-02-18 15:10:35 +0700

    update

--- dataset.py
+++ dataset.py
@@ -1,3 +1,14 @@
-import *
+from utils import *
+import os
 
 def image_dataset(image):
+    '''
+    image: jpg (./rare_disease)
+    '''
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.image_list = os.listdir(image_path)
+        self.image_list.sort()
+        self.image_list = [os.path.join(image_path, image) for image in self.image_list]
+        
+
